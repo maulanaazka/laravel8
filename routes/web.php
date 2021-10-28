9c6f6ba3fecf0c7a0f8bdd7768f6525a00ed3427
maulanaazka <maulanaazka2997@gmail.com>
2021-10-28 11:01:04 +0700

    belajar route

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// Route::view('/', 'home');
 
-Route::get('/', function () {
-    return view('welcome');
+$appName = "Laravel 8";
+Route::view('/', 'home', compact('appName'));
+// kekurangannya variable tidak bisa langsung masuk ke route nya.
+
+
+// mengarahkan ke file ..\view\home.blade.php
+# Route::get('/', fn () => view('home')); // php 7.4
+
+
+// parameter kedua untuk akses url ".../profile"
+Route::get('profile', function() {
+
+    $name = "Azka";
+
+    // compact('name') = ['name' => $name]
+    // harus sama property ('name') dan valuenya ($name)
+    return view('profile', compact('name'));
 });
