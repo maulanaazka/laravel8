8bd75bbc9ebf8cc7a9d0c9a6a6663974f78b28ca
maulanaazka <maulanaazka2997@gmail.com>
2021-10-30 19:43:35 +0700

    memahami blade komponen

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,22 @@
 @extends('layouts.app', ['title' => 'Home Page'])
 
-@section('content', 'Ini adalah halaman beranda')
+@section('content')
+    
+    {{-- anonymous component => dengan persyaratan harus ada folder components --}}
+    <x-alert>
+        <x-slot name='title'>
+            Header Component with <strong>Strong</strong> tag 
+        </x-slot>
+        
+    Ini adalah children
+
+        @slot('footer')
+            Footer
+        @endslot
+        
+    </x-alert>
+
+    <p>
+        Ini adalah halaman beranda
+    </p>
+@endsection
