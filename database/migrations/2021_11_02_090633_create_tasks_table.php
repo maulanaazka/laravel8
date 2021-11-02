4d157cbeca99de204403c440af321f34292daa3d
maulanaazka <maulanaazka2997@gmail.com>
2021-11-02 17:15:07 +0700

    migrasi pertama

---
+++ database/migrations/2021_11_02_090633_create_tasks_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id(); // bigInteger - autoIncreament - primaryKey
+            $table->string('list', 191); // string = varchar
+            $table->timestamps(); // created_at - updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
