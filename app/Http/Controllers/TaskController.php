2af996f7e1aa8932a4f82a8d14f2df567c4a8677
maulanaazka <maulanaazka2997@gmail.com>
2021-11-02 17:44:34 +0700

    membuat model pertama

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -10,35 +11,34 @@ class TaskController extends Controller
     public function index()
     {
         return view('tasks.index', [
-            'tasks' => DB::table('tasks')->orderBy('id', 'desc')->get(),
+            'tasks' => Task::orderBy('id', 'desc')->get(),
         ]);
     }
 
     public function store(Request $request) // reqquest untuk memasukkan / mengubah file dalam db
     {
-        DB::table('tasks')->insert([
-            'list' => $request->list,
-        ]);
+        Task::create(['list' => $request->list,]); // create akan memberikan feedback, sedangkan insert tidak (hanya membeikan nilai boolean)
 
         return back();
     }
 
     public function edit($id)
     {
-        $task = DB::table('tasks')->where('id', $id)->first(); // ambil data spesifik
+        // $task = Task::where('id', $id)->first(); // ambil data / fetching data secara tunggal
+        $task = Task::find($id); // untuk mencari data dengan id
         
         return view('tasks.edit', ['task' => $task]);
     }
 
     public function update(Request $request, $id)
     {
-        DB::table('tasks')->where('id', $id)->update(['list' => $request->list]);
+        Task::find($id)->update(['list' => $request->list]);
         return redirect('tasks');
     }
 
     public function destroy($id)
     {
-        $task = DB::table('tasks')->where('id', $id)->delete();
+        Task::find($id)->delete();
         return back();
     }
 }
