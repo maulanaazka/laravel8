06860988a9bf1d5ce8491131e347d0998a7ae3ff
maulanaazka <maulanaazka2997@gmail.com>
2021-11-04 11:19:14 +0700

    resource generator

---
+++ app/Models/Post.php
@@ -0,0 +1,11 @@
+<?php
+// cara buatnya make:model Post --all (-mf)
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+}
