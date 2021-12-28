94de7c03ed7b09b6b21653ffb53f38bee73619b5
Maulana Azka <maulanaazka2997@gmail.com>
2021-12-28 19:28:41 +0700

    laravel mix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,23 +7,23 @@
     <title>{{ $title }} | Code-Verse</title>
     {{-- menambah file css dari public/css/app.css --}}
     {{-- tidak menggunakan asset agar tidak selalu clear cache --}}
-    <link rel="stylesheet" href="/css/bootstrap.min.css">
-    
+    <link rel="stylesheet" href="/css/app.css">
+
     {{-- fungsi isset nya di pindahkan ke controller applayout, kecuali tidak memiliki class components --}}
     {{ $styles }}
 
 </head>
 <body>
-    
+
     {{-- memanggil file navbar.blade dalam folder components --}}
     <x-navbar></x-navbar>
     <div class="pt-4">
-        
+
         {{ $slot }}
 
     </div>
 
     {{-- menambah file js dari public/js/app.js --}}
-    <script src="/js/bootstrap.min.js"></script>
+    <script src="/js/app.js"></script>
 </body>
 </html>
